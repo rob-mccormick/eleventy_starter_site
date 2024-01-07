4348959687ff46abc309bde328c9917f74209335
Rob McCormick <robmc@hey.com>
2024-01-07 18:29:00 +0100

    Fortmat article date using Luxon in custom filter

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,15 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/styles/");
   eleventyConfig.addWatchTarget("./src/styles/");
   eleventyConfig.addPassthroughCopy("./src/images/");
   eleventyConfig.addPassthroughCopy("./src/fonts/");
   eleventyConfig.addPassthroughCopy({"./src/favicon/": "/"});
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
+  });
 
   return {
     markdownTemplateEngine: 'njk',
